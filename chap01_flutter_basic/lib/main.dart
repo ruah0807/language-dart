769597eafddaf85128be294ec39ed11ce806106f
ruah0807 <kimhk0315@gmail.com>
2024-03-14 10:49:18 +0900

    MatarialApp

--- chap01_flutter_basic/lib/main.dart
+++ chap01_flutter_basic/lib/main.dart
@@ -9,6 +9,44 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'App 타이틀',
+        //앱의 전반적인 테마를 설정한다.
+        theme: ThemeData(
+          //텍스트의 style을 미리 설정하고 호출해 사용가능
+          textTheme: TextTheme(
+            bodyText1: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('AppBar'),
+            backgroundColor: Colors.green,
+          ),
+          body: Text('Body입니다'),
+          bottomNavigationBar: BottomNavigationBar(
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.add_shopping_cart),
+                label: 'business',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.school_rounded),
+                label: 'school',
+              ),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.access_alarm),
+            onPressed: () => {
+              print('hello'),
+            },
+          ),
+        ));
   }
 }
